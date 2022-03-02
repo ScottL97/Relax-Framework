521a04ecf6a014fcf36101c06e537c3f196d1eed
Scott <2687538884@qq.com>
2022-03-03 00:19:10 +0800

    使用策略模式执行root命令，提供root用户登录执行、普通用户sudo执行、普通用户expect脚本+root密码执行三种策略，直到一种策略可行或尝试所有的策略失败

--- relax/ssh_proxy/client_proxy.py
+++ relax/ssh_proxy/client_proxy.py
@@ -8,7 +8,6 @@
 """
 import paramiko
 
-
 SSH_CONNECT_TIMEOUT = 10
 
 
@@ -22,10 +21,10 @@ class SSHClientProxy:
         self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     # TODO: 哪些是用户调用的API，要通过某种方式告诉用户
-    def connect_remote_host(self, remote_host):
+    def connect_remote_host(self, remote_host, is_use_root=False):
         self.remote_host = remote_host
         self.is_connected = False
-        return self._connect_server()
+        return self._connect_server(is_use_root)
 
     def get_transport(self):
         return self.ssh_client.get_transport()
@@ -33,7 +32,7 @@ class SSHClientProxy:
     def get_root_password(self):
         return self.remote_host.root_password
 
-    def exec_command(self, cmd, timeout=None, get_pty=False):
+    def exec_command(self, cmd, timeout=10, get_pty=False):
         return self.ssh_client.exec_command(cmd, timeout=timeout, get_pty=get_pty)
 
     def close(self):
@@ -42,15 +41,17 @@ class SSHClientProxy:
             return
         self.ssh_client.close()
 
-    def _connect_server(self):
+    def _connect_server(self, is_use_root=False):
         """
         连接SSH服务器
         :return: 0 - 成功
                 1 - 失败
         """
         try:
-            self.ssh_client.connect(self.remote_host.ip_addr, self.remote_host.port, self.remote_host.username,
-                                    self.remote_host.password, timeout=SSH_CONNECT_TIMEOUT)
+            username = self.remote_host.username if is_use_root is False else "root"
+            password = self.remote_host.password if is_use_root is False else self.remote_host.root_password
+            self.ssh_client.connect(self.remote_host.ip_addr, self.remote_host.port, username,
+                                    password, timeout=SSH_CONNECT_TIMEOUT)
         except Exception as e:
             self.log.error('ssh client connected to server failed: ' + str(e))
             return 1
--- relax/ssh_proxy/command_executor.py
+++ relax/ssh_proxy/command_executor.py
@@ -9,6 +9,7 @@
 import os
 import shutil
 import tempfile
+
 from relax.file_transfer.scp_proxy import SCPProxy
 from relax.ssh_proxy.client_proxy import SSHClientProxy
 
@@ -34,12 +35,20 @@ SU_SCRIPT = '''#!/bin/bash
 . %s/utils.sh
 exec_script_by_root "./$1"'''
 
+class SSHCommandResult:
+    def __init__(self, stdin, stdout, stderr):
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stderr
+
 
 class SSHCommandExecutor:
     def __init__(self, log):
         self.log = log
         self.ssh_client = SSHClientProxy(log)
         self.tmp_dir = tempfile.TemporaryDirectory(dir='.').name
+        self.root_cmd_executors = (self._exec_root_cmd_by_login_root, self._exec_root_cmd_by_sudo,
+                                   self._exec_root_cmd_by_expect)
 
     def __del__(self):
         self._close()
@@ -75,7 +84,11 @@ class SSHCommandExecutor:
         except Exception as e:
             self.log.error('make local tmp dir %s failed: %s' % (os.path.basename(self.tmp_dir), str(e)))
             return 1
-        stdin, stdout, stderr = self.ssh_client.exec_command("mkdir %s" % os.path.basename(self.tmp_dir))
+        try:
+            stdin, stdout, stderr = self.ssh_client.exec_command("mkdir %s" % os.path.basename(self.tmp_dir))
+        except Exception as e:
+            self.log.error('make remote tmp dir %s failed: %s' % (os.path.basename(self.tmp_dir), str(e)))
+            return 1
         err = stderr.read().decode('utf8')
         if len(err) != 0:
             self.log.error('make remote tmp dir %s failed: %s' % (os.path.basename(self.tmp_dir), err))
@@ -83,27 +96,111 @@ class SSHCommandExecutor:
 
         return 0
 
-    # 返回值：stdout字符串，stderr字符串，结果（0为成功，非0为失败）
-    def exec_cmd(self, remote_host, cmd, use_root=False):
+    def exec_cmd(self, remote_host, cmd, is_use_root=False):
+        """
+        在服务器上执行shell命令
+        :param SSHHost remote_host: 要执行命令的服务器信息
+        :param str cmd: 执行的shell命令
+        :param bool is_use_root: 是否执行root权限命令
+        :return: str: string类型的标准输出
+            str: string类型的标准错误
+            int: 返回值，1 - 失败，0 - 成功
+        """
+        if is_use_root:
+            result, err = self._exec_root_cmd(remote_host, cmd)
+        else:
+            result, err = self._exec_non_root_cmd(remote_host, cmd)
+        if err != 0:
+            return "", "", 1
+        out_string, err_string = result.stdout.read().decode('utf8'), result.stderr.read().decode('utf8')
+
+        return out_string, err_string, 0
+
+    def _exec_root_cmd_by_login_root(self, remote_host, cmd):
+        """
+        通过root用户登录执行shell命令
+        :param SSHHost remote_host: 要执行命令的服务器信息
+        :param str cmd: 执行的shell命令
+        :return: SSHCommandResult: 命令执行结果
+            int: 返回值，1 - 失败，0 - 成功
+        """
+        if self.ssh_client.connect_remote_host(remote_host, is_use_root=True) != 0:
+            self.log.error('user root connect to remote host %s:%s failed' % (remote_host.ip_addr, remote_host.port))
+            return None, 1
+        stdin, stdout, stderr = self.ssh_client.exec_command(cmd)
+        return SSHCommandResult(stdin, stdout, stderr), 0
+
+    def _exec_root_cmd_by_sudo(self, remote_host, cmd):
+        """
+        使用普通用户登录执行sudo shell命令
+        :param SSHHost remote_host: 要执行命令的服务器信息
+        :param str cmd: 执行的shell命令
+        :return: SSHCommandResult: 命令执行结果
+            int: 返回值，1 - 失败，0 - 成功
+        """
+        if self.ssh_client.connect_remote_host(remote_host) != 0:
+            self.log.error('user %s connect to remote host %s:%s failed' % (remote_host.username, remote_host.ip_addr,
+                                                                            remote_host.port))
+            return None, 1
+        cmd = "sudo " + cmd
+        try:
+            stdin, stdout, stderr = self.ssh_client.exec_command(cmd)
+        except Exception as e:
+            self.log.error("exec root cmd by sudo failed: %s" % str(e))
+            return None, 1
+        if stderr.read().startswith(b'sudo: '):
+            return None, 1
+        return SSHCommandResult(stdin, stdout, stderr), 0
+
+    def _exec_root_cmd_by_expect(self, remote_host, cmd):
+        """
+        通过expect脚本+root密码，使用普通用户登录执行shell命令
+        :param SSHHost remote_host: 要执行命令的服务器信息
+        :param str cmd: 执行的shell命令
+        :return: SSHCommandResult: 命令执行结果
+            int: 返回值，1 - 失败，0 - 成功
+        """
+        if self.ssh_client.connect_remote_host(remote_host) != 0:
+            self.log.error('user %s connect to remote host %s:%s failed' % (remote_host.username, remote_host.ip_addr,
+                                                                            remote_host.port))
+            return None, 1
+        self._create_tmp_dir()
+        self._upload_login_root_script(remote_host)
+        self._upload_script(remote_host, COMMON_SCRIPT % cmd, 'tmp.sh')
+        tmp_dir_name = os.path.basename(self.tmp_dir)
+        root_cmd = "%s/su.sh %s/tmp.sh" % (tmp_dir_name, tmp_dir_name)
+
+        stdin, stdout, stderr = self.ssh_client.exec_command(root_cmd, get_pty=True)
+        if stdout.read().find(b'/usr/bin/expect') != -1:
+            self.log.error("there is no expect on server")
+            return None, 1
+
+        return SSHCommandResult(stdin, stdout, stderr), 0
+
+    def _exec_root_cmd(self, remote_host, cmd):
+        """
+        尝试策略执行root命令，直到成功或尝试完所有的策略
+        :param SSHHost remote_host: 要执行命令的服务器信息
+        :param str cmd: 执行的shell命令
+        :return: SSHCommandResult: 命令执行结果
+            int: 返回值，1 - 失败，0 - 成功
+        """
+        for executor in self.root_cmd_executors:
+            result, err = executor(remote_host, cmd)
+            if err == 0:
+                return result, 0
+        self.log.error("exec root cmd failed")
+        return None, 1
+
+    def _exec_non_root_cmd(self, remote_host, cmd):
         # TODO: 同一host，每次执行命令都要连接一次，效率较低
         # TODO: 当连接了一台新的host时，旧的host的信息就丢失了，也就不会释放旧的host上的临时目录
         if self.ssh_client.connect_remote_host(remote_host) != 0:
             self.log.error('user %s connect to remote host %s:%s failed' % (remote_host.username, remote_host.ip_addr,
                                                                             remote_host.port))
-            return 1
-        # 执行命令
-        if use_root:
-            self._create_tmp_dir()
-            self._upload_login_root_script(remote_host)
-            self._upload_script(remote_host, COMMON_SCRIPT % cmd, 'tmp.sh')
-            tmp_dir_name = os.path.basename(self.tmp_dir)
-            root_cmd = "%s/su.sh %s/tmp.sh" % (tmp_dir_name, tmp_dir_name)
-            stdin, stdout, stderr = self.ssh_client.exec_command(root_cmd, get_pty=True)
-        else:
-            stdin, stdout, stderr = self.ssh_client.exec_command(cmd)
-        out, err = stdout.read().decode('utf8'), stderr.read().decode('utf8')
-
-        return out, err, 0
+            return None, 1
+        stdin, stdout, stderr = self.ssh_client.exec_command(cmd)
+        return SSHCommandResult(stdin, stdout, stderr), 0
 
     def _close(self):
         # TODO: 关闭窗口的时候，无法打印日志，会出异常
